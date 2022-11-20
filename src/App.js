aaa9f05774e94ee94e49623e3ff5c213f7b276b5
Rupam Sutar <rupamsutar281998@gmail.com>
2022-11-20 15:00:24 +0530

    UseEffect finally understood !

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,24 +6,47 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  console.log("App Started -------------------")
+
+  useEffect(() => {
+  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+  console.log("useEffect Started");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+      console.log("useEffect-If-statement-started");
+      console.log(`logged in effect : ${isLoggedIn}`);
+    }
+    
+  },[]);
+
+
+
 
   const loginHandler = (email, password) => {
+    console.log("LoginHandler started------------------")
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
     <React.Fragment>
+      {console.log("Fragment started !")}
+      {console.log(isLoggedIn)}
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
+      {console.log("Fragment ended !")}
     </React.Fragment>
   );
 }
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
