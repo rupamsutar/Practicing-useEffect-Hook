a9e7d9254d7962b47ed98eb2fd0a9e8c2721835e
Rupam Sutar <rupamsutar281998@gmail.com>
2022-11-25 12:00:37 +0530

    bug partially solved

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: null,
+    onLogin: (email, password) => {},
+    onLogout: () => {}
 });
+
+export const AuthContextProvider = (props) => {
+
+    // State
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Effect 
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+        if (storedUserLoggedInInformation === "1") {
+            setIsLoggedIn(true);
+        };
+    }, []);
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem("isLoggedIn", "1");
+        setIsLoggedIn(true);
+    }
+
+    const logoutHandler = () => {
+        localStorage.removeItem("isLoggedIn");
+    }
+
+    
+
+    return (
+        <AuthContext.Provider
+            value = {{
+                isLoggedIn: isLoggedIn,
+                onLogin: loginHandler,
+                onLogout: logoutHandler
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
 
 export default AuthContext;
